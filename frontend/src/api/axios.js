5fb773bb05cc855227ad08517f86604d753e9778
Leonardo Jr. R. Cabutotan <shilohjarsdel16@gmail.com>
2026-04-13 13:11:05 +0800

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 //This interceptor runs before EVERY request.
